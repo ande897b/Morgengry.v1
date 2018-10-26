c790d93c8078611e44ddb76f0ff04fb7b85c25bf
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2018-10-26 08:21:25 +0200

    Add project files.

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Morgengry.Program;
+
+namespace Morgengry
+{
+    public class Amulet
+    {
+        
+        string ItemId { get; set; }
+        string Design { get; set; }
+         Level Quality { get; set; }
+     
+
+        public Amulet(string inItemId, Level inQuality, string inDesign)
+        {
+            ItemId = inItemId;
+            Design = inDesign;
+            Quality = inQuality;
+        }
+        public Amulet(string inItemId, Level inQuality):
+        
+            this( inItemId,inQuality,"")
+        {}
+        public Amulet(string inItemId) :
+            this(inItemId,Level.medium,"")
+        { }
+
+       public override string ToString()
+        {
+
+            return "ItemId: "+ItemId +", Quality: "+Quality +", Design: "+Design;
+        }
+    }
+
+}
---
+++ Morgengry/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Book
+    {
+        string ItemId { get; set; }
+        string Title { get; set; }
+        double Price { get; set; }
+        
+
+
+
+        public Book(string inItemId, string inTitle, double inPrice)
+        {
+            ItemId = inItemId;
+            Title = inTitle;
+            Price = inPrice;
+        }
+        public Book(string inItemId, string inTitle):
+            this(inItemId, inTitle, 0)
+        { }
+        public Book (string inItemId):
+            this(inItemId, "", 0)
+        { }
+
+        public override string ToString()
+        {
+            return ("ItemId: " + ItemId + ", Title: " + Title + ", Price: " + Price);
+        }
+    }
+}
